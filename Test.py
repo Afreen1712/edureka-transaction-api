0aa6a3e13f4a51d2b369f69523413d46d04dd4f4
Afreen1712 <222430609+Afreen1712@users.noreply.github.com>
2025-08-09 18:15:03 +0530

    Added the new files & first commit

---
+++ Test.py
@@ -0,0 +1,4 @@
+(print
+    ("Hello world",
+     "First project on Edu" ,
+     "Writing first time py code"))
---
+++ Var.py
@@ -0,0 +1,5 @@
+name = "Afreen"
+age = True
+project = 1
+
+print(name, age, project)
